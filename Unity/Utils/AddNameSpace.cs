490f2277a30af88e152f89fc867923420314ca15
nmaschinskiy <nmaschinskiy@mail.ru>
2019-07-20 19:28:45 +0300

    added namespace replacer

---
+++ Unity/Utils/AddNameSpace.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AddNameSpace : UnityEditor.AssetModificationProcessor
+{
+    public static void OnWillCreateAsset(string path)
+    {
+        path = path.Replace(".meta", "");
+        int index = path.LastIndexOf(".");
+        if (index < 0)
+            return;
+
+        string file = path.Substring(index);
+        if (file != ".cs")
+            return;
+
+        index = Application.dataPath.LastIndexOf("Assets");
+        path = Application.dataPath.Substring(0, index) + path;
+        file = System.IO.File.ReadAllText(path);
+
+        string lastPart = path.Substring(path.IndexOf("Assets"));
+        string _namespace = lastPart.Substring(0, lastPart.LastIndexOf("/"));
+        _namespace = _namespace.Replace("/", ".");
+        _namespace = _namespace.Replace(" ", "");
+        _namespace = _namespace.Replace("Assets.", "");
+        _namespace = _namespace.Replace("Scripts.", "");
+
+        file = file.Replace("#NAMESPACE#", _namespace);
+        System.IO.File.WriteAllText(path, file);
+        AssetDatabase.Refresh();
+    }
+}
