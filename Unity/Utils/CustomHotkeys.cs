5c530a2be715628d2935df630b7757e7c072544b
PowerNik <nmaschinskiy@mail.ru>
2019-07-22 17:29:46 +0300

    updated CustomHotkeys.cs

--- Unity/Utils/CustomHotkeys.cs
+++ Unity/Utils/CustomHotkeys.cs
@@ -2,19 +2,19 @@ using UnityEditor;
 
 public class CustomHotkeys
 {
-    [MenuItem("Utils/Folder #1")]
+    [MenuItem("Utils/Folder #1")] // Shift + Alpha1
     static void CreateFolder()
     {
         EditorApplication.ExecuteMenuItem("Assets/Create/Folder");
     }
 
-    [MenuItem("Utils/Script #2")]
+    [MenuItem("Utils/Script #2")] // Shift + Alpha2
     static void CreateScript()
     {
         EditorApplication.ExecuteMenuItem("Assets/Create/C# Script");
     }
 
-    [MenuItem("Utils/Interface #3")]
+    [MenuItem("Utils/Interface #3")] // Shift + Alpha3
     static void CreateInterface()
     {
         EditorApplication.ExecuteMenuItem("Assets/Create/C# Interface");
