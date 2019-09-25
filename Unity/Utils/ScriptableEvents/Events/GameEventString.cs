7ea2af0217631c9e272e6cf5221eff339d54c87e
Nikolay <nmaschinskiy@mail.ru>
2019-09-25 23:34:09 +0300

    removed 'ScriptableEvents'

--- Unity/Utils/ScriptableEvents/Events/GameEventString.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-namespace Core
-{
-    [CreateAssetMenu(fileName = "GameEventString", menuName = "GameEvent/GameEvent String", order = 3)]
-    public class GameEventString : GameEvent<string> { }
-}
