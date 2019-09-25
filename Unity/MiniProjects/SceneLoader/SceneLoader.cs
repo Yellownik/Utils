2fe9c80c68d2191dd47b71c77261ee6ada64aa58
Nikolay <nmaschinskiy@mail.ru>
2019-09-25 22:09:43 +0300

    added SceneLoader as MiniProject

---
+++ Unity/MiniProjects/SceneLoader/SceneLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Dropdown Dropdown;
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        List<string> scenes = new List<string>();
+        var sceneNumber = SceneManager.sceneCountInBuildSettings;
+        for (int i = 0; i < sceneNumber; i++)
+        {
+            scenes.Add(Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i)));
+        }
+
+        Dropdown.ClearOptions();
+        Dropdown.AddOptions(scenes);
+
+        Dropdown.onValueChanged.AddListener(OnDropdownValueChanged);
+    }
+
+    private void OnDropdownValueChanged(int id)
+    {
+        SceneManager.LoadScene(Dropdown.options[id].text);
+    }
+}
