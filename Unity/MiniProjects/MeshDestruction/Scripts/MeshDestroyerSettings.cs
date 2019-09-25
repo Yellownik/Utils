3d59f781866ed0d26cd48bd48190bd868edc7719
Nikolay <nmaschinskiy@mail.ru>
2019-09-25 23:27:58 +0300

    added /MeshDestruction

---
+++ Unity/MiniProjects/MeshDestruction/Scripts/MeshDestroyerSettings.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MeshDestroyerSettings", menuName = "Destroyer/MeshDestroyerSettings")]
+public class MeshDestroyerSettings : ScriptableObject
+{
+    public Material[] materials;
+}
