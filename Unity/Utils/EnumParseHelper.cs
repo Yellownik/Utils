5e55a13a3bc813807ab70d06adc4b6edcafc6777
Nikolay <nmaschinskiy@mail.ru>
2022-09-11 15:32:46 +0300

    added EnumParseHelper.cs that caches strings when convert them to enum

---
+++ Unity/Utils/EnumParseHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+public sealed class EnumHelper<T> where T : Enum
+{
+    private static readonly ConcurrentDictionary<string, T> Cache = new(StringComparer.OrdinalIgnoreCase);
+
+    public static T Parse(string s)
+    {
+        return Cache.GetOrAdd(s, k => (T) Enum.Parse(typeof(T), k));
+    }
+}
